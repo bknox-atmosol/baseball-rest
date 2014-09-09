5aebf16763707b89d7c4c59a6a5339a303021f87
bknox-atmosol <bknox@atmosol.com>
2014-09-08 19:57:16 -0700

    Fix some initial bugs, create a package for connection.

--- server.js
+++ server.js
@@ -1,45 +1,15 @@
 var express = require('express'),
     app     = express(),
     mysql   = require('mysql'),
+    resources   = require('./lib/resources'),
     connectionpool = mysql.createPool({
       host     : 'localhost',
       user     : 'root',
       password : '',
       database : 'baseball'
     });
-res.setHeaderHeader( 'Content-Type': 'application/json' });
 app.get('/:table', function(req,res){
-  connectionpool.getConnection(function(err, connection) {
-    if (err) {
-      console.error('CONNECTION error: ', err);
-      res.statusCode = 503;
-        res.send({
-          result: 'error',
-          err: err.code
-        });
-     } else {
-       connection
-          .query('SELECT * FROM '+req.params.table+' ORDER BY id DESC LIMIT 20',
-                 req.params.id, function(err, rows, fields){
-                   if(err) {
-                     console.error('CONNECTION error: ', err);
-		     res.statusCode = 500;
-		     res.send({
-		        result: 'error',
-			error:  err.code
-		     });
-                   }
-		   res.send({
-		       result: 'success',
-		       err:    '',
-		       fields: fields,
-		       json:   rows,
-		       length: rows.length
-		   });
-		   connection.release();
-                 });
-    }
-  });
+  resources.query('SELECT * FROM '+req.params.table+' ORDER BY id DESC',connectionpool, req, res);
 });
 app.get('/:table/:id', function(req,res){});
 app.post('/:table', function(req,res){});
