05abc7e14ac6dd9bedc9ae1d1ed489a0b4477b70
bknox-atmosol <bknox@atmosol.com>
2014-09-13 15:31:05 -0700

    Create LIST, GET, and DELETE using bookshelf.js ORM.

--- server.js
+++ server.js
@@ -1,20 +1,71 @@
 var express = require('express'),
     app     = express(),
-    mysql   = require('mysql'),
-    resources   = require('./lib/resources'),
-    connectionpool = mysql.createPool({
-      host     : 'localhost',
-      user     : 'root',
-      password : '',
-      database : 'baseball'
-    });
+    bodyParser = require('body-parser'),
+    knex = require('knex')({
+      client: 'mysql',
+      connection : {
+        host:     'localhost',
+	user:     'root',
+	password: '',
+	database: 'baseball'
+      }
+    }),
+    bookshelf = require('bookshelf')(knex);
+
 app.get('/:table', function(req,res){
-  resources.query('SELECT * FROM '+req.params.table+' ORDER BY id DESC',connectionpool, req, res);
+  bookshelf.Model.extend({
+    tableName: req.params.table
+  })
+  .fetchAll().then(function(collection){
+    if(collection.length == 0) {
+      res.statusCode = 500;
+      res.send({
+         result: 'error'
+      });
+     } else {
+      res.send({
+         result: 'success',
+         err:    '',
+         json:   collection.toJSON(),
+         length: (collection.length)
+      });
+    }
+  });
 });
-app.get('/:table/:id', function(req,res){});
-app.post('/:table', function(req,res){});
+app.get('/:table/:id', function(req,res){
+  bookshelf.Model.extend({
+    tableName: req.params.table
+  })
+  .forge({id:req.params.id}).fetch().then(function(model){
+    if(model === null) {
+      res.statusCode = 500;
+      res.send({
+         result: 'error'
+      });
+     } else {
+      res.send({
+         result: 'success',
+         err:    '',
+         json:   model.toJSON()
+      });
+    }
+  });
+});
+app.post('/:table', function(req,res){
+  console.log(req.body);
+  /*bookshelf.Model.extend({
+    tableName: req.params.table
+  })
+  .forge({})*/
+});
 app.put('/:table/:id', function(req,res){});
-app.delete('/:table/:id', function(req,res){});
+app.delete('/:table/:id', function(req,res){
+  bookshelf.Model.extend({
+    tableName: req.params.table
+  })
+  .forge({id:req.params.id}).destroy();
+  res.send({result:'success'});
+});
 
 app.listen(4242);
 console.log('Baseball REST server listening on port 4242; double lucky!');
